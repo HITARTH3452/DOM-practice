e47b34d1fdc4db862145dbebc2619dbe99017c60
HITARTH3452 <hitarthsingh2201@gmail.com>
2024-01-12 23:41:08 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+// let divvv = document.getElementById("cont");
+
+// divvv.style.bachgroundColor = "yellow";
+// divvv.style.color = "blue";
+// divvv.style.margin = "20px";
+// divvv.style.padding = "10px";
+// divvv.style.fontSize = "18px";
+// divvv.style.fontWeight = "bold";
+// divvv.style.height = "200px";
+// divvv.style.width = "300px";
